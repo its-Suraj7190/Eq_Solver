ce8230b6884defd8bc6fc7aa03b598bc5edc8eab
Its-Suraj <20244098@g.deu.ac.kr>
2026-06-19 20:20:26 +0900

    Create functions.c
    Updated the includes files..

---
+++ src/functions.c
@@ -0,0 +1,12 @@
+#include"functions.h"
+#include<stdio.h>
+
+int HCF(int X, int Y){            
+ // For calcluating the HCF of A aand B.
+    while(Y){
+        int rmdr = X%Y; 
+        X = Y;
+        Y = rmdr;
+    }
+ return X;      // Returns the HCF value of A and B.
+}
