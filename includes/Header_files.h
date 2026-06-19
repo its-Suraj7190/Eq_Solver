4268243ceaf6e7fbbf5d688dea08cf9d6c6bdf7c
Its-Suraj <20244098@g.deu.ac.kr>
2026-06-19 19:57:35 +0900

    Update Header_files.h
    Updated...

--- includes/Header_files.h
+++ includes/Header_files.h
@@ -1,1 +1,9 @@
-
+int Hcf(int X, int Y ){            
+ // For calcluating the HCF of A aand B.
+    while(Y){
+        int rmdr = X%Y; 
+        X = Y;
+        Y = rmdr;
+    }
+ return X;      // Returns the HCF value of A and B.
+}
