6be9120a3ffeaf71ecfaf71bf4dc508207dfbefc
Its-Suraj <20244098@g.deu.ac.kr>
2026-06-19 21:20:55 +0900

    Update and rename St_Line_Equation.h to St_Line_Eq.h

---
+++ includes/St_Line_Eq.h
@@ -0,0 +1,6 @@
+#ifndef ST_LINE_EQ
+#define ST_LINE_EQ
+  
+void St_Line_Eq(int a, int b);
+
+#endif
--- includes/St_Line_Equation.h
+++
@@ -1,6 +0,0 @@
-#ifndef ST_LINE_EQUATION
-#define ST_LINE_EQUATION
-  
-void St_Line_Equation(int a, int b);
-
-#endif
