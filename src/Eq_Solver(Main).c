fab46392fa4a90be83e303b934e0f49e53abde9a
Its-Suraj <20244098@g.deu.ac.kr>
2026-06-23 21:11:25 +0900

    Update Eq_Solver(Main).c
    Final  Update..

--- src/Eq_Solver(Main).c
+++ src/Eq_Solver(Main).c
@@ -1,49 +1,49 @@
-#include"HCF.h"
-#include"St_Line_Eq.h"
 #include <stdio.h>
-#include <stdlib.h>                // For abs()
+#include "St_Line_Eq.h"
 
-int main(void){
+int main()
+{
+    int choice;
 
-   int user_choice;
-   char final_equation[50] = " ";
+    printf("Hi, EQ Solver is here.\n");
 
-   printf(" Hi, EQ_Solver is here to assits you.\n");
+    do
+    {
+        printf("\n1. Point-Slope Form");
+        printf("\n2. Double-Intercept Form");
+        printf("\nEnter Choice : ");
 
-   printf("\nDecide which equations to solve :\n1) Point - slope form: \n2) Double - intercept form:\n");
+        scanf("%d",&choice);
 
-   int x1, y1, x2, y2, x_intercept, y_intercept;
-   int a, b;
+    }while(choice != 1 && choice != 2);
 
-   do {
-      printf("\nEnter you choice: ");                        //  Ask for the user-choice input.. 
-      scanf("%d",&user_choice);
+    switch(choice)
+    {
+        case 1:
+        {
+            int x1, y1, x2, y2;
 
-    switch(user_choice){                                        
+            printf("\nEnter first point : ");
+            scanf("%d%d",&x1,&y1);
+            printf("Enter second point : ");
+            scanf("%d%d",&x2,&y2);
 
-      case 1:                                         //  For the Point - Slope form.
-            printf("\nEnter first point (x1, y1): ");
-            scanf("%d %d", &x1, &y1);
-            printf("Enter second point (x2, y2): ");
-            scanf("%d %d", &x2, &y2);
+            Point_Slope(x1,y1,x2,y2);
             break;
+        }
 
-      case 2:                                    //  For the Double - Intercept form.
-            printf("\nEnter X-intercept(a): ");
-            scanf("%d",&x_intercept);
-            printf("Enter Y-intercept(b): ");
-            scanf("%d",&y_intercept);
-            x1 = 0,  y1 = y_intercept,           // Expressing Y-intercept as in (x1,y1).  
-            x2 = x_intercept, y2 = 0;            // Expressing X-intercept as in (x2,y2).
-            break; 
-         
-      case 3:                                    // For the invalid choice of 1 and 2.
-            printf("Invalid choice. Enter a valid choice only (1 and 2).");   
+        case 2:
+        {
+            int a,b;
+
+            printf("\nEnter X-intercept : ");
+            scanf("%d",&a);
+            printf("Enter Y-intercept : ");
+            scanf("%d",&b);
+
+            Double_Intercept(a,b);
             break;
-    } 
-} while(!(user_choice < 3));
-
-    a = y2 - y1, b = x2 - x1;
-   St_Line_Eq(a, b);
-   return 0;
-} 
+        }
+    }
+    return 0;
+}
